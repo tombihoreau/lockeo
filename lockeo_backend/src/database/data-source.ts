68f1f661bf9287e485df0603b4b1bf626af09a7c
JulD05 <julesdemai@hotmail.fr>
2025-11-21 09:35:46 +0100

    fix fichier data-source.ts pour migration CI

--- lockeo_backend/src/database/data-source.ts
+++ lockeo_backend/src/database/data-source.ts
@@ -10,14 +10,8 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [
-    __dirname + '/../entities/*.entity.{ts,js}',
-  ],
-  migrations: [
-    __dirname + '/migrations/*.{ts,js}',
-  ],
+  entities: [__dirname + '/../entities/*.entity.{ts,js}'],
+  migrations: [__dirname + '/migrations/*.{ts,js}'],
   synchronize: false,
   migrationsTableName: process.env.DB_MIGRATIONS_TABLE || 'migrations',
 });
-
-export default AppDataSource;
