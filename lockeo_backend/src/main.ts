87001472f1638cbd6662955d8dc63cf7d92c2622
JulD05 <julesdemai@hotmail.fr>
2026-01-18 23:37:13 +0100

    vrai test version 1.0.0

--- lockeo_backend/src/main.ts
+++ lockeo_backend/src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Lockeo API')
     .setDescription('Documentation OpenAPI pour le backend Lockeo')
-    .setVersion('1.1.0')
+    .setVersion('1.0.0')
     .addBearerAuth() // JWT
     .addTag('Users') 
     .build();
